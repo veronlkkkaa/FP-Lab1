509455e489a2bbe852e41e4f73a093639c0fc672
veronlkkkaa <veronlkkkaaa@mail.ru>
2025-12-02 17:05:26 +0300

    Fix recursive and lazy solutions for Euler 9

--- src/lab1/euler9.clj
+++ src/lab1/euler9.clj
@@ -1,19 +1,19 @@
-(ns lab1.euler9
-  (:require [clojure.string :as str]))
+(ns lab1.euler9)
 
 ;; вспомогательная функция для умножения коллекции (big-int friendly)
 (defn product [coll]
   (reduce *' coll))
 
-;; 1. Хвостовая рекурсия
+;;  1. Хвостовая рекурсия 
 (defn tail-rec []
   (loop [a 1
          b 2]
-    (when-not (> a 333)
+    (if (> a 333)
+      nil
       (let [c (- 1000 a b)]
         (cond
           (> b c)
-          (recur (inc a) (inc (inc a)))
+          (recur (inc a) (+ (inc a) 1))
 
           (= (+ (* a a)
                 (* b b))
@@ -24,60 +24,70 @@
           (recur a (inc b)))))))
 
 ;; 2. Обычная рекурсия
-(defn solve-rec []
-  (loop [a 1
-         b 2]
-    (when-not (> a 333)
-      (let [c (- 1000 a b)]
-        (cond
-          (> b c)
-          (recur (inc a) (inc a))
+(defn solve-rec
+  ([] (solve-rec 1)) 
+  ([a]
+   (if (> a 333)
+     nil
+     (let [res (some (fn [b]
+                       (let [c (- 1000 a b)]
+                         (when (and (> c b)
+                                    (= (+ (* a a)
+                                          (* b b))
+                                       (* c c)))
+                           (* a b c))))
+                     (range (inc a) 500))]
+       (if res
+         res
+         (solve-rec (inc a)))))))
 
-          (= (+ (* a a)
-                (* b b))
-             (* c c))
-          (* a b c)
 
-          :else
-          (recur a (inc b)))))))
 
 ;; 3. Модульный подход
-(defn generate-triples []
+
+(defn all-triples []
   (for [a (range 1 334)
-        b (range (inc a) 500)
-        :let [c (- 1000 a b)]
-        :when (and (> c b)
-                   (= (+ (* a a)
-                         (* b b))
-                      (* c c)))]
-    [a b c]))
+        b (range (inc a) 500)]
+    [a b (- 1000 a b)]))
+
+(defn pythagorean? [[a b c]]
+  (and (> c b)
+       (= (+ (* a a)
+             (* b b))
+          (* c c))))
 
 (defn solve-modular []
-  (->> (generate-triples)
+  (->> (all-triples)      ; генерация
+       (filter pythagorean?) ; фильтрация
        first
-       (apply *)))
+       product))          ; свёртка (reduce внутри product)
 
-;; 4. Перебор общий источник данных для обеих реализаций
+;; 4. Генерация при помощи map 
 (def triples
   (->> (range 1 334)
        (mapcat (fn [a]
                  (map (fn [b]
-                        (let [c (- 1000 a b)]
-                          (when (and (> c b)
-                                     (= (+ (* a a) (* b b))
-                                        (* c c)))
-                            [a b c])))
-                      (range (inc a) 500))))
-       (remove nil?)))
+                        [a b (- 1000 a b)])
+                      (range (inc a) 500))))))
 
-;; 4 Map
 (defn solve-with-map []
-  (let [triple (first triples)]
-    (when triple
-      (apply * triple))))
+  (->> triples
+       (filter pythagorean?)   ; используем тот же предикат
+       first
+       product))
 
-;; 5 Ленивая последовательность. используем тот же ленивый перебор triples, берём ту же тройку и считаем произведение
+;; 5. Бесконечная ленивная последовательность 
+
+
+(def lazy-triples
+  (for [a (iterate inc 1)          
+        b (iterate inc (inc a))    
+        :let [c (- 1000 a b)]
+        :while (pos? c)]           ;
+    [a b c]))
+
 (defn solve-lazy []
-  (let [[a b c] (first triples)]
-    (when a
-      (* a b c))))
+  (->> lazy-triples
+       (filter pythagorean?)  
+       first
+       product))
